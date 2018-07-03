3d67bacbf945181dc9ba5604b16347bd7fc19f3a
Faisal <peysal@gmail.com>
2018-07-03 12:51:47 +0800

    class URL root mapping

---
+++ licensing-service/src/main/java/com/pey/licenses/controllers/LicenseServiceController.java
@@ -0,0 +1,10 @@
+package com.pey.licenses.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/v1/organizations/{organizationId}/licenses")
+public class LicenseServiceController {
+
+}
