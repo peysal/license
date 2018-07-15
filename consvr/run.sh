4471eb21cd484bbe10992de28db69fae72f8c7d2
Faisal <peysal@gmail.com>
2018-07-15 22:40:45 +0800

    db basic confuguration

---
+++ consvr/run.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "********************************************************"
+echo "Starting the consvr";
+echo "********************************************************"
+mvn spring-boot:run
+#curl -X http://localhost:8888/licensingservice/dev
